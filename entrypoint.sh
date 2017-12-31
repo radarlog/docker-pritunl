2961499fde87b4e148d32e575725de987782c25b
Ilian Ranguelov <me@radarlog.net>
2017-12-31 04:38:02 +0100

    use full mongodb uri

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pritunl set-mongodb mongodb://${MONGODB_SERVER}/pritunl
+pritunl set-mongodb ${MONGODB_URI}
 pritunl set app.reverse_proxy true
 pritunl set app.server_ssl false
 pritunl set app.server_port 9700
